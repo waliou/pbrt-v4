391612901c640b6d3874b98044c2f8faf086045f
waliou <brian891221@gmail.com>
2024-04-28 02:15:46 +0800

    Complete Voxel Initiation

--- src/pbrt/cpu/aggregates.h
+++ src/pbrt/cpu/aggregates.h
@@ -104,14 +104,30 @@ class KdTreeAggregate {
     Bounds3f bounds;
 };
 
+struct GridVoxel {
+    GridVoxel(std::vector<Primitive> &prims) : primitives(prims) {}
+
+    void AddPrimitive(size_t prim_id) { prims_id.push_back(prim_id); }
+
+    std::vector<size_t> prims_id;
+    std::vector<Primitive> &primitives;
+};
+
 class GridAggregate {
   public:
+    using VoxelCoordinate = Vector3i;
     // GridAggregate Public Methods
-    GridAggregate(std::vector<Primitive> p) : primitives(std::move(p)) {}
+    GridAggregate(std::vector<Primitive> prims) : primitives(std::move(prims)) {
+        calculateBounds();
+        getVoxelSetting(bounds);
+        createVoxels();
+        size_t prim_id = 0;
+        for (const auto &prim : primitives)
+            fillVoxelExtents(prim, prim_id++);
+    }
 
     static GridAggregate *Create(std::vector<Primitive> prims,
                                  const ParameterDictionary &parameters) {
-        // TODO: Init Grid, Calculate Bounds
         LOG_VERBOSE("GRID Creating!");
         LOG_VERBOSE("GRID Creating!");
         LOG_VERBOSE("GRID Creating!");
@@ -132,9 +148,69 @@ class GridAggregate {
 
   private:
     // GridAggregate Private Methods
+    void calculateBounds() {
+        for (auto p : primitives)
+            bounds = Union(bounds, p.Bounds());
+    }
+    // calculate required information for voxels
+    void getVoxelSetting(const Bounds3f &bound) {
+        // TODO
+        delta = bounds.pMax - bounds.pMin;
+        for (int axis = 0; axis < 3; ++axis) {
+            // Hardcode to 64 voxels per axis
+            nVoxel[axis] = 64;
+            width[axis] = delta[axis] / nVoxel[axis];
+            invWidth[axis] = 1.f / width[axis];
+        }
+    }
+    // create Voxels for 3 dimension
+    void createVoxels() {
+        voxels.resize(nVoxel[0]);
+        for (auto &x_voxels : voxels) {
+            x_voxels.resize(nVoxel[1]);
+            for (auto &xy_voxels : x_voxels)
+                for (int i = 0; i < nVoxel[2]; ++i)
+                    xy_voxels.emplace_back(primitives);
+        }
+    }
+
+    void fillVoxelExtents(const Primitive &p, size_t prim_id) {
+        auto primitive_bound = p.Bounds();
+        auto voxel_min = posToVoxel(primitive_bound.pMin),
+             voxel_max = posToVoxel(primitive_bound.pMax);
+        for (auto x = voxel_min.x; x <= voxel_max.x; ++x)
+            for (auto y = voxel_min.y; y <= voxel_max.y; ++y)
+                for (auto z = voxel_min.z; z <= voxel_max.z; ++z) {
+                    // // Debug
+                    // std::string msg = "Add Primitive-" + std::to_string(prim_id) +
+                    //                   " into voxel[" + std::to_string(x) + ", " +
+                    //                   std::to_string(y) + ", " + std::to_string(z) +
+                    //                   "]";
+                    // LOG_VERBOSE(msg.c_str());
+                    voxels[x][y][z].AddPrimitive(prim_id);
+                }
+    }
+
+    VoxelCoordinate posToVoxel(const Point3f &p) {
+        VoxelCoordinate voxel_coordinate;
+        for (int axis = 0; axis < 3; ++axis)
+            voxel_coordinate[axis] = posToVoxel(p, axis);
+        return voxel_coordinate;
+    }
+
+    int posToVoxel(const Point3f &pos, int axis) const {
+        auto v_pos = pstd::floor((pos[axis] - bounds.pMin[axis]) * invWidth[axis]);
+        return Clamp(v_pos, 0, nVoxel[axis] - 1);
+    }
 
     // GridAggregate Private Members
     std::vector<Primitive> primitives;
+
+    std::vector<std::vector<std::vector<GridVoxel>>> voxels;
+
+    Vector3f delta;
+    Point3f nVoxel;
+    Point3f width, invWidth;
 
     // bounds of all primitives
     Bounds3f bounds;
